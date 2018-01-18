8c417b32505f4321a9cb09e83e8fa86fb72b1be0
apolo96 <andresfpvclap@gmail.com>
2018-01-18 13:35:03 -0500

    Create factories and seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Category;
+use App\Product;
+use App\Transaction;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+//         $this->call(UsersTableSeeder::class);
+        factory(User::class, 200)->create();
+        factory(Category::class, 30)->create();
+        factory(Product::class, 1000)->create()->each(
+            function ($producto) {
+                $categorias = Category::all()->random(mt_rand(1, 5))->pluck('id');
+                $producto->categories()->attach($categorias);
+            }
+        );
+        factory(Transaction::class, 1000)->create();
     }
 }
